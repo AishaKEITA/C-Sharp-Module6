a4bacb4861996967b9d194325cf03f751978d735
aisha <aishakeita6@gmail.com>
2022-12-20 15:38:20 +0000

    nitial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Assignment6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveDataFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitAltF4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblDateTimm = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblPrority = new System.Windows.Forms.Label();
+            this.comboBoxPriority = new System.Windows.Forms.ComboBox();
+            this.lblToDo = new System.Windows.Forms.Label();
+            this.txtTodo = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBoxTodo = new System.Windows.Forms.GroupBox();
+            this.lstTask = new System.Windows.Forms.ListBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblLstPriority = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBoxTodo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(664, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.openDataToolStripMenuItem,
+            this.saveDataFileToolStripMenuItem,
+            this.exitAltF4ToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
+            this.openToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(148, 22);
+            this.openToolStripMenuItem1.Text = "New";
+            // 
+            // openDataToolStripMenuItem
+            // 
+            this.openDataToolStripMenuItem.Name = "openDataToolStripMenuItem";
+            this.openDataToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.openDataToolStripMenuItem.Text = "Open data file";
+            // 
+            // saveDataFileToolStripMenuItem
+            // 
+            this.saveDataFileToolStripMenuItem.Name = "saveDataFileToolStripMenuItem";
+            this.saveDataFileToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.saveDataFileToolStripMenuItem.Text = "Save data file";
+            // 
+            // exitAltF4ToolStripMenuItem
+            // 
+            this.exitAltF4ToolStripMenuItem.Name = "exitAltF4ToolStripMenuItem";
+            this.exitAltF4ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.exitAltF4ToolStripMenuItem.Text = "Exit   Alt + F4";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblDateTimm
+            // 
+            this.lblDateTimm.AutoSize = true;
+            this.lblDateTimm.Location = new System.Drawing.Point(25, 46);
+            this.lblDateTimm.Name = "lblDateTimm";
+            this.lblDateTimm.Size = new System.Drawing.Size(77, 13);
+            this.lblDateTimm.TabIndex = 1;
+            this.lblDateTimm.Text = "Date and Time";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(122, 43);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.Value = new System.DateTime(2022, 12, 20, 14, 11, 11, 0);
+            // 
+            // lblPrority
+            // 
+            this.lblPrority.AutoSize = true;
+            this.lblPrority.Location = new System.Drawing.Point(353, 46);
+            this.lblPrority.Name = "lblPrority";
+            this.lblPrority.Size = new System.Drawing.Size(36, 13);
+            this.lblPrority.TabIndex = 3;
+            this.lblPrority.Text = "Prority";
+            // 
+            // comboBoxPriority
+            // 
+            this.comboBoxPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPriority.FormattingEnabled = true;
+            this.comboBoxPriority.Location = new System.Drawing.Point(412, 43);
+            this.comboBoxPriority.Name = "comboBoxPriority";
+            this.comboBoxPriority.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPriority.TabIndex = 4;
+            // 
+            // lblToDo
+            // 
+            this.lblToDo.AutoSize = true;
+            this.lblToDo.Location = new System.Drawing.Point(25, 95);
+            this.lblToDo.Name = "lblToDo";
+            this.lblToDo.Size = new System.Drawing.Size(35, 13);
+            this.lblToDo.TabIndex = 5;
+            this.lblToDo.Text = "To do";
+            // 
+            // txtTodo
+            // 
+            this.txtTodo.Location = new System.Drawing.Point(66, 88);
+            this.txtTodo.Name = "txtTodo";
+            this.txtTodo.Size = new System.Drawing.Size(467, 20);
+            this.txtTodo.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(539, 88);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(66, 20);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxTodo
+            // 
+            this.groupBoxTodo.Controls.Add(this.btnDelete);
+            this.groupBoxTodo.Controls.Add(this.btnChange);
+            this.groupBoxTodo.Controls.Add(this.lblDescription);
+            this.groupBoxTodo.Controls.Add(this.lblLstPriority);
+            this.groupBoxTodo.Controls.Add(this.lblHour);
+            this.groupBoxTodo.Controls.Add(this.lblDate);
+            this.groupBoxTodo.Controls.Add(this.lstTask);
+            this.groupBoxTodo.ForeColor = System.Drawing.Color.IndianRed;
+            this.groupBoxTodo.Location = new System.Drawing.Point(12, 140);
+            this.groupBoxTodo.Name = "groupBoxTodo";
+            this.groupBoxTodo.Size = new System.Drawing.Size(609, 211);
+            this.groupBoxTodo.TabIndex = 8;
+            this.groupBoxTodo.TabStop = false;
+            this.groupBoxTodo.Text = "To do";
+            // 
+            // lstTask
+            // 
+            this.lstTask.FormattingEnabled = true;
+            this.lstTask.Location = new System.Drawing.Point(38, 37);
+            this.lstTask.Name = "lstTask";
+            this.lstTask.Size = new System.Drawing.Size(539, 121);
+            this.lstTask.TabIndex = 0;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.ForeColor = System.Drawing.Color.Black;
+            this.lblDate.Location = new System.Drawing.Point(49, 21);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Date";
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.ForeColor = System.Drawing.Color.Black;
+            this.lblHour.Location = new System.Drawing.Point(201, 21);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(30, 13);
+            this.lblHour.TabIndex = 2;
+            this.lblHour.Text = "Hour";
+            // 
+            // lblLstPriority
+            // 
+            this.lblLstPriority.AutoSize = true;
+            this.lblLstPriority.ForeColor = System.Drawing.Color.Black;
+            this.lblLstPriority.Location = new System.Drawing.Point(320, 21);
+            this.lblLstPriority.Name = "lblLstPriority";
+            this.lblLstPriority.Size = new System.Drawing.Size(38, 13);
+            this.lblLstPriority.TabIndex = 3;
+            this.lblLstPriority.Text = "Priority";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.ForeColor = System.Drawing.Color.Black;
+            this.lblDescription.Location = new System.Drawing.Point(430, 21);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblDescription.TabIndex = 4;
+            this.lblDescription.Text = "Description";
+            // 
+            // btnChange
+            // 
+            this.btnChange.ForeColor = System.Drawing.Color.Black;
+            this.btnChange.Location = new System.Drawing.Point(140, 174);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(103, 22);
+            this.btnChange.TabIndex = 5;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.ForeColor = System.Drawing.Color.Black;
+            this.btnDelete.Location = new System.Drawing.Point(323, 174);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 21);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 378);
+            this.Controls.Add(this.groupBoxTodo);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTodo);
+            this.Controls.Add(this.lblToDo);
+            this.Controls.Add(this.comboBoxPriority);
+            this.Controls.Add(this.lblPrority);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblDateTimm);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "To do reminder by ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBoxTodo.ResumeLayout(false);
+            this.groupBoxTodo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveDataFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitAltF4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDataToolStripMenuItem;
+        private System.Windows.Forms.Label lblDateTimm;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblPrority;
+        private System.Windows.Forms.ComboBox comboBoxPriority;
+        private System.Windows.Forms.Label lblToDo;
+        private System.Windows.Forms.TextBox txtTodo;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox groupBoxTodo;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblLstPriority;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.ListBox lstTask;
+    }
+}
+
---
+++ Task.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    public class Task
+    {
+        private DateTime date;//to access date and time of the task
+        private string description;//will allow the user to add the description of the task
+        private PriorityType priority;//this will give the user the priority selected
+
+
+        //default constructor
+        public Task()
+        {
+            priority = PriorityType.Normal;
+        }
+        /// <summary>
+        /// constructor with parameter
+        /// the other constructors are called using the this keyyword
+        /// </summary>
+        /// <param name="taskDate"></param>
+        public Task(DateTime taskDate) : this(taskDate, string.Empty, PriorityType.Normal)
+        {
+
+        }
+        /// <summary>
+        /// description, date, and priority are object saved here using this
+        /// </summary>
+        /// <param name="taskDate"></param>
+        /// <param name="description"></param>
+        /// <param name="priority"></param>
+        public Task(DateTime taskDate, string description, PriorityType priority)
+        {
+            this.date = taskDate;
+            this.description = description;
+            this.priority = priority;
+        }
+
+        /// <summary>
+        /// to give a read and write access to date
+        /// </summary>
+        public DateTime DateAndTime
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        /// <summary>
+        /// to give a read and write access for priority type
+        /// </summary>
+        public PriorityType Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }
+        /// <summary>
+        /// to give a read and write access to description
+        /// </summary>
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+    }
+}
